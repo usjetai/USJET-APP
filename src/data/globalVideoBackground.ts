82515e51eef61d430d83fa8a705376d37e097b0e
usjetai <ameerkarim80@gmail.com>
2026-06-27 19:19:43 -0400

    Swap global background video

--- src/data/globalVideoBackground.ts
+++ src/data/globalVideoBackground.ts
@@ -1,4 +1,4 @@
-export const GLOBAL_VIDEO_BACKGROUND_YOUTUBE_ID = "C3GHgWNXcvI" as const;
+export const GLOBAL_VIDEO_BACKGROUND_YOUTUBE_ID = "sN8mQR5KcS8" as const;
 
 export const globalVideoBackground =
   `https://www.youtube.com/watch?v=${GLOBAL_VIDEO_BACKGROUND_YOUTUBE_ID}` as const;
